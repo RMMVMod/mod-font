cfc13a8ac5801825053b450c6bc5bda1f3cb651f
Yeechan Lu <wz.bluesnow@gmail.com>
2015-12-04 13:55:33 +0800

    Add settings.js

---
+++ settings.js
@@ -0,0 +1,21 @@
+/*:
+ * @param normalFontName
+ * @desc The font name for normal.
+ * @default Segoe UI
+ *
+ * @param fixedFontName
+ * @desc The font name for fixed.
+ * @default Segoe UI
+ *
+ * @param mediumFontName
+ * @desc The font name for medium.
+ * @default Segoe UI
+ *
+ * @param heavyFontName
+ * @desc The font name for heavy.
+ * @default Segoe UI
+ *
+ * @param symbolFontName
+ * @desc The font name for symbol.
+ *
+ */
